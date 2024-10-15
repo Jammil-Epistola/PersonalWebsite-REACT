3618e1a99811c945451852ac2fef9aa19a3086f0
Jammil Epistola <epistolajammil45@gmail.com>
2024-10-15 22:03:34 +0800

    Update React App

--- epistola-personalweb/src/App.js
+++ epistola-personalweb/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation/>
+        <div style={{padding:'0 20px'}}>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Skills" element={<Skills />}/>
+            <Route path="/Contact" element={<Contact />}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
